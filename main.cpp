b5115feaf887f8fbe6cb25b6e7d28fb238355b50
Petra Gospodnetic <petra.gospodnetic@gmail.com>
2017-10-16 10:46:11 -0600

    Visualize a RGB point cloud

--- main.cpp
+++ main.cpp
@@ -2,14 +2,18 @@
 * @Author: Petra Gospodnetic
 * @Date:   2017-09-28 12:56:17
 * @Last Modified by:   Petra Gospodnetic
-* @Last Modified time: 2017-10-16 10:17:41
+* @Last Modified time: 2017-10-16 10:45:11
 */
 #include <iostream>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+#include <boost/thread/thread.hpp>
 
 int main()
 {
+    // Read in the cloud.
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (
         new pcl::PointCloud<pcl::PointXYZRGB>);
 
@@ -23,6 +27,7 @@ int main()
         << " data points from the .pcd file with the following fields: "
         << std::endl;
 
+    // Print the cloud.
     for(size_t i = 0; i < cloud->points.size(); i++)
         std::cout << "    " << cloud->points[i].x
             << " "    << cloud->points[i].y
@@ -30,5 +35,29 @@ int main()
             << " "    << cloud->points[i].rgb
             << std::endl;
 
+    //  
+    // Visualize the cloud.
+    //
+
+    // Initialize the viewer
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(
+        new pcl::visualization::PCLVisualizer("3D viewer"));
+    viewer->setBackgroundColor(0, 0, 0);
+    pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB>
+        rgb(cloud);
+    viewer->addPointCloud<pcl::PointXYZRGB>(cloud, rgb, "sample cloud");
+    viewer->setPointCloudRenderingProperties(
+        pcl::visualization::PCL_VISUALIZER_POINT_SIZE,
+        3,
+        "sample cloud");
+    viewer->addCoordinateSystem(1.0);
+    viewer->initCameraParameters();
+
+    // Run the main loop.
+    while(!viewer->wasStopped())
+    {
+        viewer->spinOnce(100);
+        boost::this_thread::sleep (boost::posix_time::microseconds(100000));
+    }
     return 0;
 }   
