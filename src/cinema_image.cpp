1ea5ada462c23a3376b3a475e0a9c773f3705839
Petra Gospodnetic <petra.gospodnetic@gmail.com>
2017-10-23 11:28:39 -0600

    Cast yaml to 2D vector

--- src/cinema_image.cpp
+++ src/cinema_image.cpp
@@ -2,7 +2,7 @@
 * @Author: Petra Gospodnetic
 * @Date:   2017-10-17 16:19:55
 * @Last Modified by:   Petra Gospodnetic
-* @Last Modified time: 2017-10-20 15:03:09
+* @Last Modified time: 2017-10-23 11:28:14
 */
 // Composite raster of .im and .png files from Cinema database into a single
 // CinemaImage class.
@@ -35,19 +35,20 @@ namespace cinema
 
     void test_read_cinema_image()
     {
+        //
+        // Read .npz file
+        //
         const std::string filename = "/home/petra/Desktop/SampleBasedReconstruction/data/rainbowsphere_C.cdb/image/phi=0/theta=0/vis=0/colorSphere1=0.npz";
         // Call python. The call path assumes we are in build folder at the time
         // execution of the SBR program.
         const std::string system_call = "python ../python_utils/npz2yaml.py -i " + filename;
         std::system(system_call.c_str());
 
-        YAML::Node primes = YAML::LoadFile(filename + ".yaml");
-        std::cout << "Primes size: " << primes.size() << "x" << primes[0].size() << std::endl;        
-
-        // Otput the data.
-        for (YAML::const_iterator row=primes.begin(); row!=primes.end(); row++) {
-            for (YAML::const_iterator col=row->begin(); col!=row->end(); col++)
-                std::cout << col->as<float>() << " ";
+        std::vector<std::vector<float>> npz_array = YAML::LoadFile(filename + ".yaml").as<std::vector<std::vector<float>>>();
+        // // Otput the data.
+        for (std::vector<std::vector<float>>::iterator row=npz_array.begin(); row!=npz_array.end(); row++) {
+            for (std::vector<float>::iterator col=row->begin(); col!=row->end(); col++)
+                std::cout << *col << " ";
         }
     }
 } // !namespace cinema
